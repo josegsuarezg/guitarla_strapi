a114eb8b7cbcc5a12dd8e0291160adc2bf189c0e
José Suárez <josegsuarezg@gmail.com>
2022-03-08 10:50:40 -0300

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'adbd70ab136c68bedf15982ab1f7a818'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '849b41a1-ccbe-43be-ac55-9a4d6d724dc3'
+};
